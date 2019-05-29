f8fbdb4759bd0dc941aaa41eac1486abec4bc6e3
Estagiario 005 <stag005@benner.com.br>
2019-05-29 10:55:23 -0300

    csharp 04 - aula 01 - Lidando com erros

--- T01/csharp4/ByteBank/Program.cs
+++ T01/csharp4/ByteBank/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ByteBank
 {
@@ -10,20 +6,77 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            int resultado = Metodo();
 
-
-            ContaCorrente conta = new ContaCorrente ( 2931, 189231 );
-
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
-
-
+            if (resultado == -2)
+            {
+                Console.WriteLine("Ocorreu um erro");
+            }
 
             Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static int Metodo()
+        {
+            int resultadoDivisao = TestaDivisao(0);
+
+            if (resultadoDivisao  == -2)
+            {
+                return -2;
+            }
+
+
+           int resultado2 = TestaDivisao(20);
+
+            if (resultado2 == -2)
+            {
+                return -2
+            }
+
+            return 0;
+        }
+
+        private static int TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            if (resultado == -2)
+            {
+                return -2;
+            }         
 
 
 
+            if (resultado == -1)
+            {
+                Console.WriteLine("Não é possivel fazer divisão por 0");
 
+            }
+            else
+            {
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
 
+            }
+
+            return 0;
+            // Numero = 10
+            // Divisor = 20
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            if(divisor == 0)
+            {
+                return -1; 
+
+            }
+            if (divisor > numero)
+            {
+                return -1;
+            }
+
+            return numero / divisor;
         }
     }
 }
