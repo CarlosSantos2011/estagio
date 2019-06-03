54c62a210d9d97d72f1b81d476f89b57cb35a595
Estagiario 005 <stag005@benner.com.br>
2019-06-03 09:28:53 -0300

    final aula 04 entity

--- T02/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T02/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,50 +11,6 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-
-                var produtos = contexto.Produtos.ToList();
-
-                foreach (var p in produtos)
-                {
-                    Console.WriteLine(p);
-                }
-                foreach (var e in contexto.ChangeTracker.Entries())
-                {
-                    Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-                }
-
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Desinfetante",
-                    Categoria = "limpeza",
-                    Preco = 2.99
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                contexto.SaveChanges();           
-                
-                ExibeEntries(contexto);
-            }
-        } 
-        
-        private static void ExibeEntries(Func<IEnumerable<EntityEntry>> entries)
-        {
-            throw new NotImplementedException();
-        }
-        
-        private static void ExibeEntries(LojaContext contexto)
-        {
-          void ExibeEntries(IEnumerable<EntityEntry> entries)
-
-            {
-                foreach (var e in contexto.ChangeTracker.Entries())
-                {
-                    Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-                }
-            }
 
         }
     }
