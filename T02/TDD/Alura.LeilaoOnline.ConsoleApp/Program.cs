b85707efeabadffdccb7f9a1c0eac2dd4e80857c
Estagiario 005 <stag005@benner.com.br>
2019-07-01 10:18:57 -0300

    começo tdd' '

---
+++ T02/TDD/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        private static void Verifica (double esperado, double obtido)
+        {
+
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Teste ok");
+            }
+            else
+            {
+                Console.WriteLine("teste falhou");
+            }
+        }
+        private static void LeilaoComVariosLances()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            //Arranje
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+       
+            //Act
+            leilao.TerminaPregao();
+
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+           
+            
+            
+        }
+        private static void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            
+
+            //Arranje
+            leilao.RecebeLance(fulano, 800);
+            //Act
+            leilao.TerminaPregao();
+
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+            Console.ReadLine();
+        }
+
+
+
+        
+    
+        static void Main(string[] args)
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+            
+        }
+    }
+}
---
+++ T02/TDD/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Text;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            //Arranje
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            //Act
+            leilao.TerminaPregao();
+
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+
+
+
+        }
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+
+            //Arranje
+            leilao.RecebeLance(fulano, 800);
+            //Act
+            leilao.TerminaPregao();
+
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+            
+        }
+    }
+}
