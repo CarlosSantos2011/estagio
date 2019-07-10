c4209d4e435004ac726157076c6e010c750e5432
Estagiario 005 <stag005@benner.com.br>
2019-07-10 11:41:57 -0300

    java

--- T03/JavaScript/js/principal.js
+++ T03/JavaScript/js/principal.js
@@ -44,3 +44,14 @@ for(var i = 0; i ,pacientes.length; i ++){
     tdImc.textContent = imc.toFixed(2);
     }
   }
+  var botaoAdicionar = document.querySelector("#adicionar-paciente");
+  botaoAdicionar.addEventListener("click", function(event) {
+      event.preventDefault();
+      console.log("Oi eu sou o botao e fui clicado");
+  });
+
+  });
+
+titulo.addEventListener("click", function (){
+  console.log ("fui clickado");
+})
