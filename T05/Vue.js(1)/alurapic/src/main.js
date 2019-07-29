2d3506422ac75c8baba88575948bbcc41a5fba9b
Estagiario 005 <stag005@benner.com.br>
2019-07-29 11:18:21 -0300

    A

--- T05/Vue.js(1)/alurapic/src/main.js
+++ T05/Vue.js(1)/alurapic/src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
-import VueResource from 'vue-resource'
+import { routes } from './routes';
 
-Vue.use(VueResource)
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes, 
+  mode: 'history'
+});
+
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
