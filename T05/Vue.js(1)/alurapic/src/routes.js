b03763450e2a89fcc260939752ab33e18de25395
Estagiario 005 <stag005@benner.com.br>
2019-08-02 11:32:32 -0300

    vue2

--- T05/Vue.js(1)/alurapic/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/home/Home.vue';
-import Cadastro from './components/cadastro/Cadastro.vue';
-
-export const routes = [
-
-    { path: '', component: Home,titulo: 'Home' },
-    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
-
-];
